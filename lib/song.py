6e79ff28dd3e9694a95b51b3efdbf22405909ec9
Umurowoto <umurowoto132@gmail.com>
2025-08-17 20:53:44 +0300

    class atribute and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,56 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment song count
+        Song.add_song_to_count()
+        
+        # Add to genres and artists
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        
+        # Update genre and artist counts
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+    
+    @classmethod
+    def add_song_to_count(cls):
+        """Increment the song count by 1"""
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Add genre to genres list if not already present"""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Add artist to artists list if not already present"""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Increment count for the given genre"""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        """Increment count for the given artist"""
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
